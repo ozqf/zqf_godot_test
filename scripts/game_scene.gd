81ded344a83d4e571e2d4c1acb5279bff9941cb8
zqf <zqf@zealousquakefan.com>
2020-02-08 15:51:29 +0000

    Trying to move camera control out of game scene

--- scripts/game_scene.gd
+++ scripts/game_scene.gd
@@ -5,20 +5,11 @@ var _playerPrefab = preload("res://prefabs/Player.tscn")
 
 var _spawnMax: float = 1
 var _spawnTick: float = 1
-var cam: Camera2D
-var plyr: Node2D
 
 func _ready():
 	print("Begin game scene")
-	cam = $Camera2D
-	plyr = $Player
-	print("Camera: " + str(cam))
-	print("Player: " + str(plyr))
 
 func _process(delta):
-	cam.position = plyr.get_node("Body").position
-	var camTxt: String = "CamPos: " + str(cam.position)
-	globals.debugCamPos = camTxt
 	if _spawnTick >= _spawnMax:
 		_spawnTick = _spawnMax
 		#var enemy = _enemyPrefab.instance()
