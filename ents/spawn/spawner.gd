0421ecda0bbe9e40e113dab9bcee65c5ea658c24
zqf <zqf@zealousquakefan.com>
2020-03-12 21:04:59 +0000

    Added basic mob spawner

---
+++ ents/spawn/spawner.gd
@@ -0,0 +1,18 @@
+extends Spatial
+
+var spawnTime: float = 2
+var spawnTick: float = 1
+
+func tock():
+	var mob = factory.create_mob()
+	var pos:Vector3 = self.transform.origin
+	factory.add_to_scene_root(mob, pos)
+	mob.transform.origin = pos
+	print("Spawn mob at " + str(pos))
+
+func _process(_delta: float):
+	if spawnTick <= 0:
+		spawnTick = spawnTime
+		tock()
+	else:
+		spawnTick -= _delta
--- service/factory.gd
+++ service/factory.gd
@@ -6,6 +6,9 @@ extends Node
 var projectile_def_t = load("res://ents/projectile/projectile_def.gd")
 var projectile_t = preload("res://ents/projectile/ent_projectile_3d.tscn")
 
+var mob_t = preload("res://ents/mobs/ent_mob.tscn")
+
+
 
 ###########################################################################
 # factory
@@ -15,3 +18,10 @@ func create_projectile_def():
 
 func create_projectile():
 	return projectile_t.instance()
+
+func create_mob():
+	return mob_t.instance()
+
+func add_to_scene_root(obj: Spatial, pos: Vector3):
+	get_tree().get_root().add_child(obj)
+	obj.transform.origin = pos
